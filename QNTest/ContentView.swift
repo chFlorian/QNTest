63f9adb2b9adbfaf1a7548f22f53ffec1b066c44
Florian Schweizer <post.an.flo@gmail.com>
2021-10-01 17:54:04 +0200

    Add QuickNote support

--- QNTest/ContentView.swift
+++ QNTest/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentActivity: NSUserActivity? = nil
+    @State var text = "Empty String"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(text)
+                .padding()
+            
+            Button("Start Activity") {
+                startActivity()
+            }
+        }
+        .onContinueUserActivity("fschweizer.QNTest.DummyActivity") { activity in
+            if let userInfo = activity.userInfo,
+               let helloString = userInfo["hello"] as? String {
+                text = helloString
+            }
+        }
+        .onDisappear {
+            stopActivity()
+        }
+    }
+    
+    func startActivity() {
+        let activity = NSUserActivity(activityType: "fschweizer.QNTest.DummyActivity")
+        activity.title = "You're doing a dummy activity."
+        activity.persistentIdentifier = "DummyActivity"
+        
+        activity.userInfo = ["hello": "world"]
+        
+        currentActivity = activity
+        currentActivity?.becomeCurrent()
+    }
+    
+    func stopActivity() {
+        currentActivity?.resignCurrent()
     }
 }
 
